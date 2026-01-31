108b241242986db0b29e9d3ca3ed99e2cf1a0f99
Yash <156162167+Yash08238@users.noreply.github.com>
2026-01-31 22:59:27 +0530

    Update home.tskjhfw

--- Frontend/src/app/home/home.ts
+++ Frontend/src/app/home/home.ts
@@ -6,7 +6,7 @@ import { CommonModule } from '@angular/common';
     standalone: true,
     imports: [CommonModule],
     templateUrl: './home.html',
-    styleUrl: './home.css',
+    styleUrls: ['./home.css'],
 })
 export class HomeComponent {
     title = 'mean-project';
